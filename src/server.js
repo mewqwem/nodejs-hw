6fa3c555c351180bc046eab352a042ffbe6d91a7
Oleh <roadto12mmr@gmail.com>
2026-05-16 19:29:35 +0200

    finish second homework

--- src/server.js
+++ src/server.js
@@ -1,79 +1,37 @@
 import express from 'express';
 import cors from 'cors';
-import pino from 'pino-http';
 import 'dotenv/config';
 
+import { connectMongoDB } from './db/connectMongoDB.js';
+
+import { logger } from './middleware/logger.js';
+import { notFoundHandler } from './middleware/notFoundHandler.js';
+import { errorHandler } from './middleware/errorHandler.js';
+
+import notesRoutes from './routes/notesRoutes.js';
+
 const app = express();
+const PORT = process.env.PORT ?? 3001;
 
-//? middlewares
-app.use((req, res, next) => {
-  console.log(`Time: ${new Date().toLocaleString()}`);
-  next();
-});
-
-app.use(express.json());
-app.use(cors());
+app.use(logger);
 app.use(
-  pino({
-    level: 'info',
-    transport: {
-      target: 'pino-pretty',
-      options: {
-        colorize: true,
-        translateTime: 'HH:MM:ss',
-        ignore: 'pid,hostname',
-        messageFormat:
-          '{req.method} {req.url} {res.statusCode} - {responseTime}ms',
-        hideObject: true,
-      },
-    },
+  express.json({
+    type: ['application/json', 'application/vnd.api+json'],
   }),
 );
+app.use(cors());
 
-//! handlers
-app.get('/', (req, res) => {
-  res.status(200).json({ message: 'Hello World!' });
-});
-
-app.get('/notes', (req, res) => {
-  res.status(200).json({
-    message: 'Retrieved all notes',
-  });
-});
-
-app.get('/notes/:noteId', (req, res) => {
-  const { noteId } = req.params;
-
-  res.status(200).json({
-    message: `Retrieved note with ID: ${noteId}`,
-  });
-});
-
-app.get('/test-error', () => {
-  throw new Error('Simulated server error');
-});
+//! routes
+app.use(notesRoutes);
 
 //! 404
-app.use((req, res) => {
-  res.status(404).json({
-    message: `Route not found on method: ${req.method} 😭😭😭`,
-  });
-});
+app.use(notFoundHandler);
 
 //! error
-app.use((err, req, res, next) => {
-  console.error(err);
+app.use(errorHandler);
 
-  const isProd = process.env.NODE_ENV === 'production';
-
-  res.status(500).json({
-    message: isProd
-      ? 'Something went wrong. Please try again later.'
-      : err.message,
-  });
-});
-
-const PORT = process.env.PORT ?? 3000;
+//! connect to DB
+await connectMongoDB();
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
