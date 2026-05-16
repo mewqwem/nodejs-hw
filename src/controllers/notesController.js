12dc958e153363fe0544d82d4dee674a368e276a
Oleh <roadto12mmr@gmail.com>
2026-05-16 21:30:13 +0200

    fix little issues

--- src/controllers/notesController.js
+++ src/controllers/notesController.js
@@ -1,7 +1,7 @@
 import createHttpError from 'http-errors';
 import { Note } from '../models/note.js';
 
-export const getNotes = async (req, res) => {
+export const getAllNotes = async (req, res) => {
   const notes = await Note.find();
   res.status(200).json(notes);
 };
@@ -11,7 +11,7 @@ export const getNoteById = async (req, res) => {
   const note = await Note.findById(noteId);
 
   if (!note) {
-    throw createHttpError(404, 'Student not found');
+    throw createHttpError(404, 'Note not found');
   }
 
   res.status(200).json(note);
--- src/server.js
+++ src/server.js
@@ -11,7 +11,7 @@ import { errorHandler } from './middleware/errorHandler.js';
 import notesRoutes from './routes/notesRoutes.js';
 
 const app = express();
-const PORT = process.env.PORT ?? 3001;
+const PORT = process.env.PORT ?? 3000;
 
 app.use(logger);
 app.use(
