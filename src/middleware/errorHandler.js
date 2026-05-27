c8bdffecae3c843bbda5a8d3483203b3f43b4498
Oleh <roadto12mmr@gmail.com>
2026-05-27 09:53:51 +0200

    add validations, pagination and filters

--- src/middleware/errorHandler.js
+++ src/middleware/errorHandler.js
@@ -1,7 +1,22 @@
 import { HttpError } from 'http-errors';
+import { isCelebrateError } from 'celebrate';
 
 export const errorHandler = (err, req, res, next) => {
   console.error('Error Middleware:', err);
+
+  if (isCelebrateError(err)) {
+    const details = [];
+    err.details.forEach((detail) => {
+      details.push({
+        message: detail.message,
+        path: detail.path.join('.'),
+      });
+    });
+    return res.status(400).json({
+      message: 'Validation error',
+      details,
+    });
+  }
 
   if (err instanceof HttpError) {
     return res.status(err.status).json({
