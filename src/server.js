7680cc1c5aa3b20093c2eb71c0e2fd7fd4635e1c
Oleh <roadto12mmr@gmail.com>
2026-05-30 13:27:21 +0200

    add auth

--- src/server.js
+++ src/server.js
@@ -10,6 +10,7 @@ import { logger } from './middleware/logger.js';
 import { notFoundHandler } from './middleware/notFoundHandler.js';
 import { errorHandler } from './middleware/errorHandler.js';
 
+import authRoutes from './routes/authRoutes.js';
 import notesRoutes from './routes/notesRoutes.js';
 
 const app = express();
@@ -24,6 +25,7 @@ app.use(
 app.use(cors());
 
 //! routes
+app.use(authRoutes);
 app.use(notesRoutes);
 
 //! celebrate errors handler
